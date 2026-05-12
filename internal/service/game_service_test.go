7407c9b5a355f4f78669c94b1aa300607af14fe8
Mikhail Mikhaylov <mmihaylovms@yandex.ru>
2026-05-12 21:37:43 +0300

    Добавлен файл тестов сервиса GameService

---
+++ internal/service/game_service_test.go
@@ -0,0 +1,17 @@
+package service_test
+
+import "testing"
+
+// TODO реализовать unit-тесты для сервиса GameService
+// TestCreateGame_Success
+// TestCreateGame_RoomNotFound
+// TestStartGame_Success
+// TestStartGame_AlreadyStarted
+// TestSubmitAnswer_Success
+// TestSubmitAnswer_GameNotActive
+// TestFinishGame_Success
+// TestFinishGame_GameNotActive
+
+func TestCreateGame_Success(t *testing.T) {
+	t.Skip()
+}
